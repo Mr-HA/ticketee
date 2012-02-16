38f661d411a0d6a8eb992f90e2a16c5b9079955f
Mr-HA <mr.ha.facebook@gmail.com>
2012-02-16 17:09:52 +0800

    Implemented creating tickets for a project

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120214092042) do
+ActiveRecord::Schema.define(:version => 20120216073149) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "tickets", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "project_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end
